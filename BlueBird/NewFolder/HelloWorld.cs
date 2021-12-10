70378f98aac70defecd85c54a99a86225db3ed04
Filipe Bravos <filipe.bravos@dtidigital.com.br>
2021-12-10 13:36:48 -0300

    Commit projeto BlueBird

---
+++ BlueBird/NewFolder/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueBird.NewFolder
+{
+    public class HelloWorld
+    {
+        public static void Hello(string[] args)
+        {
+            Console.WriteLine("HelloWorld, HelloWorld");
+            Console.ReadLine();
+        }
+        
+    }
+}
